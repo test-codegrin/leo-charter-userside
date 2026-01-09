dcb46417516d4d08ec750d6be47bada5f8a27733
Devan <devanbhensdadiya123@gmail.com>
2026-01-09 13:58:07 +0530

    Terms Updated

--- src/app/terms-conditions/page.tsx
+++ src/app/terms-conditions/page.tsx
@@ -6,7 +6,9 @@ import TermsAndConditions from "@/components/TermsAndConditions";
 export default function PaymentPage() {
   return (
     <>
-    <TermsAndConditions/>
+    <div className="w-full max-w-3xl mx-auto md:p-10 p-6">
+        <TermsAndConditions/>
+    </div>
     </>
   );
 }
